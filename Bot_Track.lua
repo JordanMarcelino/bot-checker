84bb036aa36debf74f5acfbbe5793fd641df931b
JordanMarcelino <jordangamers25@gmail.com>
2022-10-27 00:48:03 +0700

    edit bot

--- Bot_Track.lua
+++ Bot_Track.lua
@@ -1,27 +1,12 @@
 Rdp = {
-    { nordp = 1, botname = "Bot1", Status = "Nonaktif" },
-    { nordp = 1, botname = "Bot1", Status = "Nonaktif" },
-    { nordp = 2, botname = "Bot2", Status = "Nonaktif" },
-    { nordp = 2, botname = "Bot2", Status = "Nonaktif" },
-    { nordp = 3, botname = "Bot3", Status = "Nonaktif" },
-    { nordp = 3, botname = "Bot3", Status = "Nonaktif" },
-    { nordp = 4, botname = "Bot4", Status = "Nonaktif" },
-    { nordp = 4, botname = "Bot4", Status = "Nonaktif" },
-    { nordp = 5, botname = "Bot5", Status = "Nonaktif" },
-    { nordp = 5, botname = "Bot5", Status = "Nonaktif" },
-    { nordp = 6, botname = "Bot6", Status = "Nonaktif" },
-    { nordp = 6, botname = "Bot6", Status = "Nonaktif" }
+    { nordp = 1, botname = "ZAAWARUDOO84", Status = "Nonaktif" },
+    { nordp = 1, botname = "ZAAWARUDOO85", Status = "Nonaktif" },
+    { nordp = 1, botname = "ZAAWARUDOO86", Status = "Nonaktif" },
 }
 
-ShowIP = false
+ShowIP = true
 ip_add = {
-    "192.168.0.1",
-    "192.168.0.2",
-    "192.168.0.3",
-    "192.168.0.4",
-    "192.168.0.5",
-    "192.168.0.6",
-    "192.168.0.7"
+    "192.168.0.1"
 }
 
 hookURL = "https://discord.com/api/webhooks/1034843021897895998/wLnTxByg1HolNQ2WGqkBc92Que72AqVVPc5EGb5sfkVeF8IbrUU9BfuC1oJDuraa8Fgw"
@@ -32,4 +17,4 @@ Teks_Footer = "Jangan Kepo Ya Adick Adick"
 Emote_Offline = "<:offline:982957367392088124>"
 Emote_Online = "<:online:982957403756716052>"
 
-menit = 10 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
+menit = 5 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
