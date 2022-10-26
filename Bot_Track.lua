236fdd63fb94ceeca589f82df16007aac62439ae
JordanMarcelino <jordangamers25@gmail.com>
2022-10-27 00:11:37 +0700

    change name

---
+++ Bot_Track.lua
@@ -0,0 +1,35 @@
+Rdp = {
+    { nordp = 1, botname = "Bot1", Status = "Nonaktif" },
+    { nordp = 1, botname = "Bot1", Status = "Nonaktif" },
+    { nordp = 2, botname = "Bot2", Status = "Nonaktif" },
+    { nordp = 2, botname = "Bot2", Status = "Nonaktif" },
+    { nordp = 3, botname = "Bot3", Status = "Nonaktif" },
+    { nordp = 3, botname = "Bot3", Status = "Nonaktif" },
+    { nordp = 4, botname = "Bot4", Status = "Nonaktif" },
+    { nordp = 4, botname = "Bot4", Status = "Nonaktif" },
+    { nordp = 5, botname = "Bot5", Status = "Nonaktif" },
+    { nordp = 5, botname = "Bot5", Status = "Nonaktif" },
+    { nordp = 6, botname = "Bot6", Status = "Nonaktif" },
+    { nordp = 6, botname = "Bot6", Status = "Nonaktif" }
+}
+
+ShowIP = false
+ip_add = {
+    "192.168.0.1",
+    "192.168.0.2",
+    "192.168.0.3",
+    "192.168.0.4",
+    "192.168.0.5",
+    "192.168.0.6",
+    "192.168.0.7"
+}
+
+hookURL = "https://discord.com/api/webhooks/1034843021897895998/wLnTxByg1HolNQ2WGqkBc92Que72AqVVPc5EGb5sfkVeF8IbrUU9BfuC1oJDuraa8Fgw"
+
+Teks_Deskripsi = "List Nick Para Babu"
+Teks_Footer = "Jangan Kepo Ya Adick Adick"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 10 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
